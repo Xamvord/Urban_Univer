78963948b24b2afd5a38816adff4e246ad3aeff0
Xamvord <169102628+Xamvord@users.noreply.github.com>
2024-11-06 01:32:04 +0300

    Update hw_m13_5b.py

--- Module13/hw_m13_5b.py
+++ Module13/hw_m13_5b.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-api = "7350487384:AAEr9S7ZYrlkUQSHGQh4vy7tNZgPIOpGzFY"
+api = "..."
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
